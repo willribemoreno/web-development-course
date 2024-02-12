f0dd935bab4c9aabcbd298ff1dd932a7e2773e32
William Moreno <william.ribeiro.moreno@gmail.com>
2024-02-12 17:02:38 -0300

    [feat] Add section 17 + challenge

---
+++ Section 17/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,27 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var path = "./images/dice" + randomNumber1 + ".png";
+var path2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll('.dice img')[0].setAttribute("src", path);
+document.querySelectorAll('.dice img')[1].setAttribute("src", path2);
+
+
+var dice1 = document.querySelectorAll('img')[0].getAttribute("src");
+var dice2 = document.querySelectorAll('img')[1].getAttribute("src");
+console.log('Dice1: ' + dice1);
+console.log('Dice2: ' + dice2);
+
+if (randomNumber1 > randomNumber2) {    
+    document.querySelector('h1').innerHTML = 'Player 1 Wins!'
+}
+
+if (randomNumber1 < randomNumber2) {    
+    document.querySelector('h1').innerHTML = 'Player 2 Wins!'
+}
+
+
+if (randomNumber1 === randomNumber2) {    
+    document.querySelector('h1').innerHTML = 'Its a tie!'
+}
